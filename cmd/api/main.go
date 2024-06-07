6c21ec2b62e1d606e5c99102911ad3c6ce6490dd
wongchunkang <chunkang.wong94@gmail.com>
2024-06-07 23:27:23 +0800

    Implemented activateUserHandler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -60,7 +60,7 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
-	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "6dd8719a08d3f2", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "7b53f516927068", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.wck.net>", "SMTP sender")
