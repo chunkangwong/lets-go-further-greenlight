f238f96764180ea64b351b07e31d3c603efbadd0
wongchunkang <chunkang.wong94@gmail.com>
2024-06-09 23:21:39 +0800

    Recorded http method

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/felixge/httpsnoop"
 	"golang.org/x/time/rate"
 	"greenlight.wck.net/internal/data"
 	"greenlight.wck.net/internal/validator"
@@ -193,18 +195,17 @@ func (app *application) metrics(next http.Handler) http.Handler {
 	totalRequestsReceived := expvar.NewInt("total_requests_received")
 	totalResponsesSent := expvar.NewInt("total_responses_sent")
 	totalProcessingTimeMicroSeconds := expvar.NewInt("total_processing_time_μs")
+	totalResponsesSentByStatus := expvar.NewMap("total_responses_sent_by_status")
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
 		totalRequestsReceived.Add(1)
 
-		next.ServeHTTP(w, r)
+		metrics := httpsnoop.CaptureMetrics(next, w, r)
 
 		totalResponsesSent.Add(1)
 
-		duration := time.Since(start).Microseconds()
+		totalProcessingTimeMicroSeconds.Add(metrics.Duration.Microseconds())
 
-		totalProcessingTimeMicroSeconds.Add(duration)
+		totalResponsesSentByStatus.Add(strconv.Itoa(metrics.Code), 1)
 	})
 }
